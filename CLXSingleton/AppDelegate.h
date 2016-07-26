fe813225937a6f1982626ec1798fb5301eb95b85
liangxiu.chen <liangxiu.chen.cn@gmail.com>
2016-07-26 22:58:50 +0800

    Initial Commit

---
+++ CLXSingleton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLXSingleton
+//
+//  Created by liangxiu.chen on 16/7/26.
+//  Copyright © 2016年 liangxiu.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CLXSingleton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CLXSingleton
+//
+//  Created by liangxiu.chen on 16/7/26.
+//  Copyright © 2016年 liangxiu.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
